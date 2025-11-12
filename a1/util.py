412da2ec34b09ead47ec4426953bf6636a8945f7
s4totrin <s4totrin@uni-trier.de>
2025-11-12 20:21:06 +0100

    test init

--- a1/util.py
+++ a1/util.py
@@ -2,6 +2,8 @@ def generate_burger_orders(n: int) -> list:
     """
     Generates n burger orders based on the predefined distributions.
 
+    \\ Todo
+
     :param n: number of burger orders to generate
     """
     raise NotImplementedError('Has not yet been implemented')
