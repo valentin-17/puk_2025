5aa678697f82a1cdfb01f1889718f5a50bc5573a
s4totrin <s4totrin@uni-trier.de>
2025-11-20 17:58:39 +0100

    Added Vallis Burger creation and time working time

--- a1/util.py
+++ a1/util.py
@@ -9,21 +9,24 @@ from typing import (
     Callable
 )
 from heapq import heappush
+import config as c
 
 np.random.seed(42)      # set the seed
-SIM_DURATION = 210 # von 10:00 bis 14:30
+SIM_DURATION = 100 # von 11:00 bis 14:30
 
 
 
 class DynamicDeadlineItem(NamedTuple):
     """Wrap an arbitrary *item* with an order-able *deadline*.
-
     """
     burger: Any
 
     deadline: Any
 
     env: Any
+
+    def get_burger(self):
+        return self.burger
 
     def get_deadline(self):
         return self.deadline - self.env.now
@@ -36,7 +39,7 @@ class DynamicDeadlineItem(NamedTuple):
         return priorty_1 < priorty_2
 
 
-class PriorityFilterStore(Store):
+class PriorityFilterStore(FilterStore):
 
     def _do_put(self, event: StorePut) -> Optional[bool]:
         if len(self.items) < self._capacity:
@@ -44,12 +47,6 @@ class PriorityFilterStore(Store):
             event.succeed()
         return None
     
-    def get(
-        self, filter: Callable[[Any], bool] = lambda item: True
-    ) -> FilterStoreGet:
-        """Request to get an *item*, for which *filter* returns ``True``,
-        out of the store."""
-        return FilterStoreGet(self, filter)
     
 
 class BestellungList:
@@ -75,8 +72,8 @@ class WarmSchrank:
     def __init__(self, env, delay):
         self.env = env
         self.delay = delay
-        #self.store = PriorityFilterStore(env) # sum = -1864 for p_of_failure = 0.5
-        self.store = FilterStore(env) # sum = -1871 for p_of_failure = 0,5 
+        self.store = PriorityFilterStore(env) # sum = -1864 for p_of_failure = 0.5
+        #self.store = FilterStore(env) # sum = -1871 for p_of_failure = 0,5 
 
     def latency(self, value):
         yield self.env.timeout(self.delay)
@@ -92,13 +89,12 @@ class WarmSchrank:
 def Student(env, bestellung_list):
     
     while env.now < 180:
-
-        burger_creation = DynamicDeadlineItem(["warmeZutaten",2,3,4,5], deadline=env.now+30, env=env)   # TODO set random burger generation from statistics
-        # Burgers have *at least 2 but at max 20 ingredients*
+        
+        burger_creation = DynamicDeadlineItem(create_burger(), deadline=env.now+30, env=env)
         bestellung_list.put(burger_creation)
-        #print("bestellt at ", env.now)
-        #print("deadline",burger_creation.get_deadline())
-        yield env.timeout(2)    # Time between orders in seconds: *Normal(mean=105, scale=8)* 
+        print("bestellt at ", env.now)
+        delay = np.random.normal(loc=105, scale=8) / 60
+        yield env.timeout(delay)
 
 
 def Zuarbeiter(env, bestellung_list, warm_schrank):
@@ -106,11 +102,10 @@ def Zuarbeiter(env, bestellung_list, warm_schrank):
     while True:
 
         bestellung = yield bestellung_list.get()
-
-        yield env.timeout(5) # TODO Warm ingredients have a preparation time of *Uniform(360, 600) seconds (i.e. 6 - 10 min.)* independent of total 
-        # TODO something like this: yield env.timeout(get_preperation_time(bestellung['warmeZutaten']))
-        # TODO Worker needs *Gamma(shape=10, scale=2) seconds (i.e. 10 - 30 sec.)* to take ingredients from freezer
-        # TODO Burger is assembled *at max 5 minutes before pick up time*
+        delay = np.random.gamma(shape=10, scale=2) / 60
+        yield env.timeout(delay)
+        delay = np.random.uniform(low=360, high=600) / 60
+        yield env.timeout(delay)
         warm_schrank.put(bestellung)
 
 
@@ -118,36 +113,58 @@ def Burgermeister(env, bestellung_list, warm_schrank):
     sum = 0
     while True:             
         
-        # TODO Cold ingredients take *Normal(mean=5, scale=1) seconds (i.e. approx. 5 sec.)*
-        yield env.timeout(0.083)
+        delay = np.random.normal(loc=5, scale=1) / 60
+        yield env.timeout(delay)
+
         warme_zutaten = yield warm_schrank.get(lambda x: x.get_deadline() < 5)
-        
-        print("PRIO",warme_zutaten.get_deadline())
+        print("deadline lower 5: ", warme_zutaten.get_deadline())
+        print(warme_zutaten.get_burger())
 
         if np.random.rand() <= 0.5:
-        #if np.random.rand() <= 0.5:
             print("Fehler der Zuberetiung")
             bestellung_list.put(warme_zutaten)
         else:
-            #print(f'Deliverd at time: {env.now}')
-            print("etikitieren")
-            # TODO etikitieren
+
+            print("etikitieren zur deadline: ", warme_zutaten.get_deadline())
             if warme_zutaten.get_deadline() < 0:
                 sum += warme_zutaten.get_deadline()
-
+            delay = np.random.uniform(low=10, high=20) / 60
+            yield env.timeout(delay)
+            # if pommes
+            delay = np.random.uniform(low=15, high=30) / 60
+            yield env.timeout(delay)
         print("sum", sum)
 
         
-        
+
+burger_id = 1        
+def create_burger():
+    global burger_id
+    while True:
+        burger = {
+        'bun': np.random.binomial(c.BUN_N, c.BUN_P),
+        'patty': np.random.binomial(c.PATTY_N, c.PATTY_P),
+        'bacon': np.random.binomial(c.BACON_N, c.BACON_P),
+        'salad': np.random.binomial(c.SALAD_N, c.SALAD_P),
+        'sauce': np.random.binomial(c.SAUCE_N, c.SAUCE_P),
+        'spice': np.random.poisson(c.SPICE_MU),
+        'cheese': np.random.poisson(c.CHEESE_MU),
+        'vegetables': np.random.poisson(c.VEGGIE_MU),
+        }
+        total_ingredients = sum(burger.values())
+        if (c.MIN_INGREDIENTS <= total_ingredients <= c.MAX_INGREDIENTS and
+                    burger['bun'] > 0 and
+                    burger['patty'] > 0):
+                burger['id'] = burger_id
+                burger_id += 1
+                return burger
 
 
 
-# Setup and start the simulation
-#print('Event Latency')
 env = simpy.Environment()
 
-bestellung_list = BestellungList(env, 10)
-warm_schrank = WarmSchrank(env, 5)
+bestellung_list = BestellungList(env, 5)
+warm_schrank = WarmSchrank(env, 10)
 env.process(Student(env, bestellung_list))
 env.process(Zuarbeiter(env, bestellung_list, warm_schrank))
 env.process(Zuarbeiter(env, bestellung_list, warm_schrank))
